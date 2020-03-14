15bc576b08ed954c9082619c0df4d431f839f67b
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-14 19:50:48 +0900

    added about page

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -1,8 +1,38 @@
 <template>
-  <div id="menu-background">
+  <div id="about-background">
     <Nav />
     <div class="mobile-container">
-      <p>About amu</p>
+      <div id="salon-info">
+        <table>
+          <thead>
+            <tr>
+              <th colspan="2">About</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>サロン名</td>
+              <td>amu</td>
+            </tr>
+            <tr>
+              <td>代表</td>
+              <td>Tom</td>
+            </tr>
+            <tr>
+              <td>住所</td>
+              <td>山梨県甲府市xxxx ビル2F</td>
+            </tr>
+            <tr>
+              <td>TEL</td>
+              <td>xxxx-xx-xxxx</td>
+            </tr>
+            <tr>
+              <td>Open</td>
+              <td>10:00 - 19:00</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -19,9 +49,50 @@ export default {
 </script>
 
 <style scoped>
-#menu-background {
+#about-background {
   width: 100%;
   height: 100vh;
-  background-color: #bfb8a0;
+  animation: background-slide 60s linear infinite;
+}
+@keyframes background-slide {
+  0% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_1.jpg") no-repeat center/cover;
+  }
+  25% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_1.jpg") no-repeat center/cover;
+  }
+  25.01% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_2.jpg") no-repeat center/cover;
+  }
+  50% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_2.jpg") no-repeat center/cover;
+  }
+  50.01% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_3.jpg") no-repeat center/cover;
+  }
+  75% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_3.jpg") no-repeat center/cover;
+  }
+  75.01% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_4.jpg") no-repeat center/cover;
+  }
+  100% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/about_4.jpg") no-repeat center/cover;
+  }
+}
+#salon-info {
+  color: #fff;
+  background-color: rgba(255, 255, 255, 0.3);
+  padding: 16px;
+  border-radius: 5px;
+  height: 70vh;
 }
 </style>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -7,50 +7,58 @@
 </template>
 
 <script>
-import HomeMessage from '../modules/HomeMessage.vue'
-import Nav from '../modules/Nav.vue'
-import ScrollDown from '../modules/ScrollDown.vue'
+import HomeMessage from "../modules/HomeMessage.vue";
+import Nav from "../modules/Nav.vue";
+import ScrollDown from "../modules/ScrollDown.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     HomeMessage,
     Nav,
     ScrollDown
   }
-}
+};
 </script>
 
 <style scoped>
-  #home-background {
-    width: 100%;
-    height: 100vh;
-    animation: home-background 60s linear 6s infinite;
+#home-background {
+  width: 100%;
+  height: 100vh;
+  animation: background-slide 60s linear 6s infinite;
+}
+@keyframes background-slide {
+  0% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_1.jpg") no-repeat center/cover;
   }
-  @keyframes home-background {
-    0% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_1.jpg') no-repeat center/cover;
-    }
-    25% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_1.jpg') no-repeat center/cover;
-    }
-    25.01% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_2.jpg') no-repeat center/cover;
-    }
-    50% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_2.jpg') no-repeat center/cover;
-    }
-    50.01% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_3.jpg') no-repeat center/cover;
-    }
-    75% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_3.jpg') no-repeat center/cover;
-    }
-    75.01% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_4.jpg') no-repeat center/cover;
-    }
-    100% {
-      background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)), url( '../../assets/home_4.jpg') no-repeat center/cover;
-    }
+  25% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_1.jpg") no-repeat center/cover;
   }
+  25.01% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_2.jpg") no-repeat center/cover;
+  }
+  50% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_2.jpg") no-repeat center/cover;
+  }
+  50.01% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_3.jpg") no-repeat center/cover;
+  }
+  75% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_3.jpg") no-repeat center/cover;
+  }
+  75.01% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_4.jpg") no-repeat center/cover;
+  }
+  100% {
+    background: linear-gradient(rgba(47, 23, 15, 0.6), rgba(47, 23, 15, 0.6)),
+      url("../../assets/home_4.jpg") no-repeat center/cover;
+  }
+}
 </style>
