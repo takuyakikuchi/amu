a31259810081925a860aac3986f527e11bee5b12
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-14 14:36:05 +0900

    added media queries to gallery

--- src/components/modules/GalleryModal.vue
+++ src/components/modules/GalleryModal.vue
@@ -1,45 +1,46 @@
 <template>
   <div class="modal" v-show="!hide">
     <Close @click.native="$emit('hideModal')"></Close>
-    <img class="modal-content" :src="targetSrc">
+    <img class="modal-content" :src="targetSrc" />
     <p>{{ id }}</p>
   </div>
 </template>
 
 <script>
-  import Close from '../modules/Close.vue';
+import Close from "../modules/Close.vue";
 
-  export default {
-    props: ['id'],
-    computed: {
-      targetSrc() {
-        return require(`../../assets/gallery_${parseInt(this.id)}.jpg`);
-      }
-    },
-    name: 'GalleryModal',
-    components: {
-      Close
+export default {
+  props: ["id"],
+  computed: {
+    targetSrc() {
+      return require(`../../assets/gallery_${parseInt(this.id)}.jpg`);
     }
+  },
+  name: "GalleryModal",
+  components: {
+    Close
   }
+};
 </script>
   
 <style scoped>
-  .modal {
-    position: absolute;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100vh;
-    overflow: auto;
-    background-color: rgba(0,0,0,0.9);
-    display: flex;
-  }
-  .modal-content {
-    margin: auto;
-    display: block;
-    width: 80%;
-    height: 50vh;
-    border-radius: 5px;
-  }
+.modal {
+  position: absolute;
+  z-index: 2;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.9);
+  display: flex;
+}
+.modal-content {
+  margin: auto;
+  display: block;
+  width: 80%;
+  height: 50vh;
+  border-radius: 5px;
+  max-width: 415px;
+}
 </style>
--- src/components/pages/Gallery.vue
+++ src/components/pages/Gallery.vue
@@ -1,13 +1,45 @@
 <template>
   <div>
-    <!-- <Nav /> -->
-    <div id="gallery-container">
-      <img src="../../assets/gallery_1.jpg" alt="gallery 1" class="img-card" @click="showModal(1)">
-      <img src="../../assets/gallery_2.jpg" alt="gallery 2" class="img-card" @click="showModal(2)">
-      <img src="../../assets/gallery_3.jpg" alt="gallery 3" class="img-card" @click="showModal(3)">
-      <img src="../../assets/gallery_4.jpg" alt="gallery 4" class="img-card" @click="showModal(4)">
-      <img src="../../assets/gallery_5.jpg" alt="gallery 5" class="img-card" @click="showModal(5)">
-      <img src="../../assets/gallery_6.jpg" alt="gallery 6" class="img-card" @click="showModal(6)">
+    <Nav />
+    <div class="responsive-container">
+      <div id="gallery-container">
+        <img
+          src="../../assets/gallery_1.jpg"
+          alt="gallery 1"
+          class="img-card"
+          @click="showModal(1)"
+        />
+        <img
+          src="../../assets/gallery_2.jpg"
+          alt="gallery 2"
+          class="img-card"
+          @click="showModal(2)"
+        />
+        <img
+          src="../../assets/gallery_3.jpg"
+          alt="gallery 3"
+          class="img-card"
+          @click="showModal(3)"
+        />
+        <img
+          src="../../assets/gallery_4.jpg"
+          alt="gallery 4"
+          class="img-card"
+          @click="showModal(4)"
+        />
+        <img
+          src="../../assets/gallery_5.jpg"
+          alt="gallery 5"
+          class="img-card"
+          @click="showModal(5)"
+        />
+        <img
+          src="../../assets/gallery_6.jpg"
+          alt="gallery 6"
+          class="img-card"
+          @click="showModal(6)"
+        />
+      </div>
     </div>
     <GalleryModal @hideModal="modalShow = false" v-show="modalShow" :id="id" />
     <ScrollDown />
@@ -15,14 +47,14 @@
 </template>
 
 <script>
-// import Nav from '../modules/Nav.vue';
-import ScrollDown from '../modules/ScrollDown.vue';
-import GalleryModal from '../modules/GalleryModal.vue';
+import Nav from "../modules/Nav.vue";
+import ScrollDown from "../modules/ScrollDown.vue";
+import GalleryModal from "../modules/GalleryModal.vue";
 
 export default {
-  name: 'Gallery',
+  name: "Gallery",
   components: {
-    // Nav,
+    Nav,
     ScrollDown,
     GalleryModal
   },
@@ -30,55 +62,53 @@ export default {
     return {
       modalShow: false,
       id: 1
-    }
+    };
   },
   methods: {
     showModal(id) {
       this.id = id;
-      this.modalShow = true
+      this.modalShow = true;
     }
   },
   created() {
-    this.$on(this.modalShow = false);
+    this.$on((this.modalShow = false));
   }
-}
+};
 </script>
   
 <style scoped>
+.responsive-container {
+  align-content: center;
+}
+#gallery-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-row-gap: 5px;
+}
+.img-card {
+  width: 160px;
+  max-width: 100%;
+  height: 180px;
+  place-self: center;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.img-card:hover {
+  opacity: 0.7;
+}
+@media (min-width: 500px) {
   #gallery-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-column-gap: 8px;
-    grid-row-gap: 10px;
+    grid-template-columns: 1fr 1fr 1fr;
   }
   .img-card {
-    height: 200px;
-    width: 180px;
-    place-self: center;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: 300ms;
+    width: 240px;
+    height: 260px;
   }
-  .img-card:hover {
-    opacity: 0.7;
+}
+@media (min-width: 1000px) {
+  #gallery-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
   }
-  .modal {
-    position: absolute;
-    z-index: 1;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100vh;
-    overflow: auto;
-    background-color: rgba(0,0,0,0.9);
-    display: flex;
-  }
-  .modal-content {
-    margin: auto;
-    display: block;
-    width: 80%;
-    height: 50vh;
-    border-radius: 5px;
-  }
+}
 </style>
